463d823ae774f23e706aaac9151e8b0b44e994a2
Chhayank Sharma <93913184+ChhayankCreator@users.noreply.github.com>
2022-09-05 23:22:02 +0530

    addition function

--- functions.py
+++ functions.py
@@ -3,3 +3,18 @@
 #You can pass data, known as parameters, into a function.
 
 #A function can return data as a result.
+''' In Python a function is defined using the def keyword:   '''
+
+'''3 type of function'''
+# In-built Functions  -> int(), str(), bool()
+# Module Functions    -> import math
+# User-defined Functions 
+
+'''
+def function_name (parameters):
+    // do something
+'''
+def addition(num_one,num_two):
+    print(num_one+num_two)
+
+addition(2,6)    
