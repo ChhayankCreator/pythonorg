a1ed6464bac8fea24f1fa3129df22b94effd32fa
Chhayank Sharma <93913184+ChhayankCreator@users.noreply.github.com>
2022-09-06 08:28:42 +0530

    Variable Value assign Methods

--- input.py
+++ input.py
@@ -1,3 +1,15 @@
+
+'''Python allows you to assign values to multiple variables in one line:
+
+Example
+x, y, z = "Orange", "Banana", "Cherry"'''
+
+
+'''And you can assign the same value to multiple variables in one line:
+
+Example
+x = y = z = "Orange"
+'''
 a = input('Enter Your Name: ')
 a = str(a) 
 
